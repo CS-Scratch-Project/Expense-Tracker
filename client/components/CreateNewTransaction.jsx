cf6a6369f9bc216a73c1e4f27f6dfaa817776ac1
An <104526811+AnLelol@users.noreply.github.com>
2022-07-30 22:13:09 -0500

    update

--- client/components/CreateNewTransaction.jsx
+++ client/components/CreateNewTransaction.jsx
@@ -2,11 +2,31 @@ import { ReactDOM } from "react";
 import React, { Component } from "react";
 
 const TransactionCreater = () => {
-// onClick function
+// onClick create new transaction 
 
-  function transactionEvent(){
+  // function newTransaction(){
     // grabbing the text inputted to the form
-    
+
+    const name = document.getElementById('name')
+    const date = document.getElementById('date')
+    const amount = document.getElementById('amount')
+    const entry = document.getElementById('entry')
+    const transactionComplete = document.getElementById('transactionComplete')
+    const people = document.getElementById('people')
+    const eventName_id = document.getElementById('eventName_id')
+
+    const body = {
+      name,
+      date,
+      amount,
+      entry,
+      transactionComplete,
+      people,
+      eventName_id
+    }
+
+
+
   }
 
   return (
@@ -15,7 +35,19 @@ const TransactionCreater = () => {
         <form className="eventInputForm">
           <input id="eventName" placeholder="new transaction name">
           </input>
-          <button id="submitEvent">Create</button>
+          <input id="name" placeholder="name ">
+          </input>
+          <input id="date" placeholder="day of Event">
+          </input>
+          <input id="amount" placeholder="amount">
+          </input>
+          <input id="entry" placeholder="cost">
+          </input>
+          <input id="transactionComplete" placeholder="completed transaction">
+          </input>
+          <input id="people" placeholder="name of people">
+          </input>
+          <button id="submitEvent" onClick={newTransaction}>Create</button>
         </form>
       </div>
 
