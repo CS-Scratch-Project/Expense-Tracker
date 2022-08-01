b3c5b11363d1ac63109b55762419a0f9d827c940
An <104526811+AnLelol@users.noreply.github.com>
2022-07-31 22:00:42 -0500

    update new transactions

--- client/components/CreateNewTransaction.jsx
+++ client/components/CreateNewTransaction.jsx
@@ -8,25 +8,31 @@ const TransactionCreater = () => {
    function newTransaction(){
     // grabbing the text inputted to the form
     const name = document.getElementById('name').value
+    console.log(name)
+    // const newName = document.getElementById('name')
+    // console.log(newName)
     const date = document.getElementById('date').value
+    console.log(date)
     const amount = document.getElementById('amount').value
+    console.log(amount)
     const entry = document.getElementById('entry').value
-    const transactionComplete = document.getElementById('transactionComplete').value
+    console.log(entry)
     const people = document.getElementById('people').value
-    const eventName_id = document.getElementById('eventName_id').value
-
+    console.log(people)
+    // const eventName_id = document.getElementById('eventName_id').value
+    const eventName_id = 'rubber duckies'
     const body = {
       name,
       date,
       amount,
       entry,
-      transactionComplete,
+      transactionComplete: false,
       people,
       eventName_id
     }
-    console.log(body)
+    // console.log(body)
     //asyc function to make post request
-    console.log('make new transactions function')
+    console.log('make new transactions')
     const postEvent = async () => {
       const settings = {
         method: 'POST',
@@ -37,7 +43,7 @@ const TransactionCreater = () => {
       }
       try {
         // waiting for katie to make end point
-        const response = await fetch('/moreApis/addtransact', settings)
+        const response = await fetch('/moreapis/addtransact', settings)
         const data = await response.json()
         //this is a post request to make a new transaction event, so we dont need to 
         //do anything with the response
@@ -45,7 +51,7 @@ const TransactionCreater = () => {
         console.log('something went wrong with the Create New Transaction post request')
       }
     }
-    console.log(body)
+    // console.log(body)
     postEvent()
   }
   
@@ -58,8 +64,8 @@ const TransactionCreater = () => {
       <h1>New Transaction Creator</h1>
       <div>
         <form className="eventInputForm">
-          <input id="eventName_id" placeholder="Event name">
-          </input>
+          {/* <input id="eventName_id" placeholder="Event name">
+          </input> */}
           <input id="name" placeholder="item name ">
           </input>
           <input id="date" placeholder="day of Event">
@@ -67,8 +73,6 @@ const TransactionCreater = () => {
           <input id="amount" placeholder="amount">
           </input>
           <input id="entry" placeholder="entry">
-          </input>
-          <input id="transactionComplete" placeholder="completed transaction">
           </input>
           <input id="people" placeholder="name of peoples">
           </input>
