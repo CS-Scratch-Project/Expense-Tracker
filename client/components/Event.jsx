32abbdc7726c01e86af5d25b33a12dff3838badf
Will Mack <wyllyammack@gmail.com>
2022-07-31 22:50:10 -0500

    working on pulling in info for dynamically rendering transactions

--- client/components/Event.jsx
+++ client/components/Event.jsx
@@ -1,10 +1,9 @@
-import { ReactDOM } from "react";
-import React, { Component } from "react";
-import Transaction from "./Transaction.jsx"
-import TransactionCreater from "./CreateNewTransaction.jsx";
+import { ReactDOM } from 'react';
+import React, { Component } from 'react';
+import Transaction from './Transaction.jsx';
+import TransactionCreater from './CreateNewTransaction.jsx';
 
 // const Event = () => {
-
 
 // //{}
 //   return (
@@ -17,25 +16,55 @@ import TransactionCreater from "./CreateNewTransaction.jsx";
 //   )
 // }
 
+// date = {transactionObj.date} amount = {transactionObj.amount} entry= {transactionObj.entry} transactionComplete = {transactionObj.transactionComplete} people = {transactionObj.people} eventName_id = {transactionObj.eventName_id}
+
 class Event extends Component {
   constructor(props) {
-    super(props)
-    
+    super(props);
   }
 
   render() {
+    // const [transactions, setTransactions] = useState([]);
+    // useEffect(() => {
+    //   const getData = async () => {
+    //     try {
+    //       const response = await fetch('/api'); //figure out the end route
+    //       const data = await response.json();
+    //       setTransactions(data);
+    //     } catch (err) {
+    //       console.log(`something went wrong: ${err}`);
+    //     }
+    //   };
+    //   getData();
+    // }, []);
+
+    // const transactionList = transactions.map((transactionObj, i) => {
+    //   return (
+    //     <Transaction
+    //       key={transactionObj.name}
+    //       name={transactionObj.name}
+    //       date={transactionObj.date}
+    //       amount={transactionObj.amount}
+    //       entry={transactionObj.entry}
+    //       transactionComplete={transactionObj.transactionComplete}
+    //       people={transactionObj.people}
+    //       eventName_id={transactionObj.eventName_id}
+    //     />
+    //   );
+    // });
     console.log(this.props.eventName);
     console.log(this.props.eventDescription);
     return (
-      <div className="event">
+      <div className='event'>
         <h4>{this.props.eventName}</h4>
         <p>{this.props.eventDescription}</p>
         <TransactionCreater />
-        <Transaction />
-        <Transaction />
+        {/* <Transaction />
+        <Transaction /> */}
+        {/* {transactionList} */}
       </div>
-    )
+    );
   }
 }
 
-export default Event
+export default Event;
